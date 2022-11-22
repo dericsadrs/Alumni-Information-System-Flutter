c27ec42cccf502e5e35020669c99f009d2946d28
dericsadrs <drccsadrs@gmail.com>
2022-11-22 23:55:59 +0800

    update quick

--- lib/screens/gallery/galleryPost.dart
+++ lib/screens/gallery/galleryPost.dart
@@ -29,6 +29,29 @@ class _UploadImageState extends State<UploadImage> {
   }
 
   /// Get from Camera
+
+/*Future _uploadImage(File imageFile) async{
+// ignore: deprecated_member_use
+var stream= new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+var length= await imageFile.length();
+var uri = Uri.parse("http://10.0.2.2/foodsystem/uploadg.php");
+
+var request = new http.MultipartRequest("POST", uri);
+
+var multipartFile = new http.MultipartFile("image", stream, length, filename: basename(imageFile.path));
+
+request.files.add(multipartFile);
+request.fields['productname'] = controllerName.text;
+request.fields['productprice'] = controllerPrice.text;
+request.fields['producttype'] = controllerType.text;
+request.fields['product_owner'] = globals.restaurantId;
+
+var respond = await request.send();
+if(respond.statusCode==200){
+  print("Image Uploaded");
+}else{
+  print("Upload Failed");
+}*/
   _getFromCamera() async {
     PickedFile? pickedFile = await ImagePicker().getImage(
       source: ImageSource.camera,
@@ -138,7 +161,9 @@ class _UploadImageState extends State<UploadImage> {
             height: 25,
           ),
           GestureDetector(
-              onTap: () {},
+              onTap: () {
+                print(imageFile!.path);
+              },
               child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 100.0),
                   child: Container(
